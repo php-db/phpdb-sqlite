6091e3836acc5731e00775e36c2d8252a0f9afbc
Joey Smith <jsmith@webinertia.net>
2026-03-28 11:40:07 -0500

    Removes the originally never present DDL decorators from this package. These decorators were never present in the upstream laminas-db repo. Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Sql/Platform.php
+++ src/Sql/Platform.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PhpDb\Sqlite\Sql;
 
-use PhpDb\Sql\Ddl\AlterTable;
-use PhpDb\Sql\Ddl\CreateTable;
 use PhpDb\Sql\Platform\AbstractPlatform;
 use PhpDb\Sql\Select;
 
@@ -14,7 +12,5 @@ final class Platform extends AbstractPlatform
     public function __construct()
     {
         $this->setTypeDecorator(Select::class, new SelectDecorator());
-        $this->setTypeDecorator(CreateTable::class, new Ddl\CreateTableDecorator());
-        $this->setTypeDecorator(AlterTable::class, new Ddl\AlterTableDecorator());
     }
 }
